9187970471a8c464285d108b561a6ab6d4726be8
Logan Deree <dereel@wit.edu>
2023-03-30 15:06:49 -0400

    Updated DexcomConnect with encoded URL

--- frontend/src/components/DexcomConnect.jsx
+++ frontend/src/components/DexcomConnect.jsx
@@ -1,19 +1,17 @@
+import React from "react";
 
+const DexcomConnect = () => {
 
-
-const DexcomConnect= () => {
-
-
-const dexcomLogin = async () => {
-    window.location.href="https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=FzbQyNRMDTm8xdRrcR2STg8I7S781RC0&redirect_uri=http://localhost:8080/&response_type=code&scope=offline_access";
-
-    };
+  const dexcomLogin = () => {
+    const url = `https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=${encodeURIComponent("FzbQyNRMDTm8xdRrcR2STg8I7S781RC0")}&redirect_uri=${encodeURIComponent("http://localhost:8080/")}&response_type=code&scope=${encodeURIComponent("offline_access")}`;
+    window.location.assign(url);
+  };
 
 
   return (
     <form className="box" onSubmit={dexcomLogin}>
-      <button className="button is-primary : button">
-          Connect with Dexcom
+      <button type="submit" className="button is-primary">
+        Connect with Dexcom
       </button>
     </form>
   );
