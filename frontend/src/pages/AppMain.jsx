694ff8e30e253f6c68714a524754e9baaae9d418
Logan Deree <dereel@wit.edu>
2023-04-05 11:38:29 -0400

    Deleted Sidebar and reverted AppMain

--- frontend/src/pages/AppMain.jsx
+++ frontend/src/pages/AppMain.jsx
@@ -1,48 +1,52 @@
-import React, { useContext, useEffect, useState } from "react";
-import { UserContext } from "../context/UserContext";
+import React, {useContext, useEffect, useState} from "react";
+import {UserContext} from "../context/UserContext";
 import Header from "../components/Header";
 import Register from "../components/Register";
 import Login from "../components/Login";
 import ForgotPassword from "../components/ForgotPassword";
-import Sidebar from "../components/Sidebar";
 
 const AppMain = () => {
-  const [message, setMessage] = useState("");
-  const { authToken, dexConnect } = useContext(UserContext);
-  const [token,] = authToken;
-  const [dexConnected,] = dexConnect;
+    const [message, setMessage] = useState("");
+    const {authToken, dexConnect} = useContext(UserContext);
+    const [token, setToken] = authToken;
+    const [dexConnected, setDexConnected] = dexConnect;
 
-  const getWelcomeMessage = async () => {
-    const requestOptions = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const getWelcomeMessage = async () => {
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const response = await fetch("/api/api", requestOptions);
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.log("something messed up");
+        } else {
+            setMessage(data.message);
+        }
     };
-    const response = await fetch("/api/api", requestOptions);
-    const data = await response.json();
 
-    if (!response.ok) {
-      console.log("something messed up");
-    } else {
-      setMessage(data.message);
-    }
-  };
+        const checkDexcomToken = async () => {
+        const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  useEffect(() => {
-    getWelcomeMessage();
-  }, []);
+    };
 
-  const [showRegister, setShowRegister] = useState(false);
-  const [showLogin, setShowLogin] = useState(true);
-  const [showForgotPassword, setShowForgotPassword] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(false);
+    useEffect(() => {
+        getWelcomeMessage();
+    }, []);
 
-  useEffect(() => {
-    setShowSidebar(dexConnected);
-  }, [dexConnected]);
+    const [showRegister, setShowRegister] = useState(false);
+    const [showLogin, setShowLogin] = useState(true);
+    const [showForgotPassword, setShowForgotPassword] = useState(false);
 
-  return (
+   return (
     <>
       <Header title={message} />
       <div className="columns is-centered is-mobile">
@@ -88,27 +92,11 @@ const AppMain = () => {
               {showForgotPassword && <ForgotPassword />}
             </>
           ) : (
-            <div className="has-text-centered">
-              {!dexConnected ? (
-                <a
-                  href="https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=FzbQyNRMDTm8xdRrcR2STg8I7S781RC0&redirect_uri=http://localhost:8080/VerifyDexcom/&response_type=code&scope=offline_access"
-                  className="button is-primary"
-                >
-                  {" "}
-                  Connect to Dexcom{" "}
-                </a>
-              ) : (
-                <>
-                  <button
-                    className="button is-primary"
-                    onClick={() => setShowSidebar(!showSidebar)}
-                  >
-                    ☰
-                  </button>
-                  {showSidebar && <Sidebar />}
+            <>
+                <div className="has-text-centered">
+              <a href="https://sandbox-api.dexcom.com/v2/oauth2/login?client_id=FzbQyNRMDTm8xdRrcR2STg8I7S781RC0&redirect_uri=http://localhost:8080/VerifyDexcom/&response_type=code&scope=offline_access" className="button is-primary"> Connect to Dexcom </a>
+                    </div>
                 </>
-              )}
-            </div>
           )}
         </div>
       </div>
