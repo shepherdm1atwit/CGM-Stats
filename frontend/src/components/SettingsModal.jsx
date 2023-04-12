2210f8d3afaf0c261d479ff015b0420c5820d8b1
Logan Deree <dereel@wit.edu>
2023-04-11 21:35:10 -0400

    Rewrote SettingsModal to be more concise

--- frontend/src/components/SettingsModal.jsx
+++ frontend/src/components/SettingsModal.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import DisconnectDexcomButton from "./DisconnectDexcomButton";
 import ErrorMessage from "./ErrorMessage";
-import {UserContext} from "../context/UserContext";
+import { UserContext } from "../context/UserContext";
 
 const SettingsModal = ({ onClose }) => {
   const [isActive, setIsActive] = useState(true);
-  const {authToken,} = useContext(UserContext);
-  const [token,] = authToken;
-  const [errorMessage, setErrorMessage] =  useState("");
+  const { authToken } = useContext(UserContext);
+  const [token] = authToken;
+  const [errorMessage, setErrorMessage] = useState("");
   const [maximumGlucose, setMaximumGlucose] = useState("");
   const [minimumGlucose, setMinimumGlucose] = useState("");
 
@@ -16,83 +16,100 @@ const SettingsModal = ({ onClose }) => {
     onClose();
   };
 
-  const handleMaxChange = (event) => {
-    setMaximumGlucose(event.target.value);
-  };
-
-  const handleMinChange = (event) => {
-    setMinimumGlucose(event.target.value);
-  };
-
-  //TODO: this component/piece not currently functional, disconnect dexcom button works, settings do not
-  const handleSubmit = async () => {
-    if (maximumGlucose < minimumGlucose) {
-      setErrorMessage("Maximum glucose value must be higher than minimum glucose value");
+  const handleChange = async (event) => {
+    const { name, value } = event.target;
+    if (name === "maximumGlucose") {
+      setMaximumGlucose(value);
+    } else if (name === "minimumGlucose") {
+      setMinimumGlucose(value);
     }
-    else {
+  };
+    //Function currently doesn't work
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Max"+maximumGlucose)
+    console.log("Min"+minimumGlucose)
+    if (maximumGlucose <= minimumGlucose) {
+      console.log("Max"+maximumGlucose)
+    console.log("Min"+minimumGlucose)
+      setErrorMessage("Maximum glucose value must be higher than minimum glucose value");
+    } else {
       const requestOptions = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer " + token
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ minimum: minimumGlucose, maximum: maximumGlucose })
+        body: JSON.stringify({ minimum: minimumGlucose, maximum: maximumGlucose }),
       };
       const response = await fetch("/api/savepreferences", requestOptions);
       if (!response.ok) {
         setErrorMessage("Error sending preferences to backend.");
-      }
-      else{
+      } else {
         setErrorMessage("");
       }
     }
   };
+  const test = () => {
+    console.log("Max"+maximumGlucose)
+    console.log("Min"+minimumGlucose)
+    console.log("Max > Min"+maximumGlucose > minimumGlucose )
+  }
 
   return (
     <div className={`modal ${isActive ? "is-active" : ""}`}>
       <div className="modal-background" onClick={closeModal}></div>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Settings</p>
-            <button className="delete" aria-label="close" onClick={closeModal}></button>
-          </header>
-          <form onSubmit={handleSubmit}>
-            <section className="modal-card-body">
-              <div className="field">
-                <label className="label">Maximum Preferred Blood Glucose (mg/dL)</label>
-                <div className="control">
-                  <input
-                    type="number"
-                    value={maximumGlucose}
-                    onChange={handleMaxChange}
-                    className="input"
-                    required
-                  />
-                </div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Settings</p>
+          <button className="delete" aria-label="close" onClick={closeModal}></button>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <section className="modal-card-body">
+            <div className="field">
+              <label className="label">Maximum Preferred Blood Glucose (mg/dL)</label>
+              <div className="control">
+                <input
+                  type="number"
+                  name="maximumGlucose"
+                  value={maximumGlucose}
+                  onChange={handleChange}
+                  className="input"
+                  required
+                />
               </div>
-              <div className="field">
-                <label className="label">Minimum Preferred Blood Glucose (mg/dL)</label>
-                <div className="control">
-                  <input
-                    type="number"
-                    value={minimumGlucose}
-                    onChange={handleMinChange}
-                    className="input"
-                    required
-                  />
-                </div>
+            </div>
+            <div className="field">
+              <label className="label">Minimum Preferred Blood Glucose (mg/dL)</label>
+              <div className="control">
+                <input
+                  type="number"
+                  name="minimumGlucose"
+                  value={minimumGlucose}
+                  onChange={handleChange}
+                  className="input"
+                  required
+                />
               </div>
-              <ErrorMessage message={errorMessage} color="red" />
-              <footer className="modal-card-foot">
-                <button className="button is-primary" type="submit">Save changes</button>
-                <button className="button" onClick={closeModal}>Cancel</button>
-                <DisconnectDexcomButton class="is-justify-content-right" />
-              </footer>
-            </section>
-          </form>
-        </div>
+            </div>
+            <ErrorMessage message={errorMessage} color="red" />
+            <footer className="modal-card-foot">
+              <button className="button is-primary" type="submit">
+                Save changes
+              </button>
+              <button className="button" onClick={closeModal}>
+                Cancel
+              </button>
+              <DisconnectDexcomButton class="is-justify-content-right" />
+              <button onClick={test}>
+                Test
+              </button>
+            </footer>
+          </section>
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default SettingsModal;
