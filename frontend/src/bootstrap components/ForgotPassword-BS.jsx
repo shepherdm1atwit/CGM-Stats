262a94a9415903cb2b29b837f5d161381750c5e7
Simon O'Shea <osheas1@wit.edu>
2023-06-08 14:38:24 -0400

    added new dependencies for react-bootstrap and react-router-bootstrap
    created folder for BS components to live in

---
+++ frontend/src/bootstrap components/ForgotPassword-BS.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import ErrorMessage from "../components/ErrorMessage";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const submitReset = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email }),
+    };
+
+    const response = await fetch("/api/resetrequest", requestOptions);
+    const data = await response.json();
+
+    if (!response.ok) {
+      setSuccessMessage("");
+      setErrorMessage("An error has occurred.");
+    } else {
+      setSuccessMessage("Please check your email for reset link.");
+      setErrorMessage("");
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    submitReset();
+
+    };
+
+
+  return (
+     <div className="column">
+      <form className="box" onSubmit={handleSubmit}>
+        <h1 className="title has-text-centered">Forgot Password</h1>
+        <div className="field">
+          <label className="label">Email Address</label>
+          <div className="control">
+            <input
+              type="email"
+              placeholder="Enter email"
+              autoComplete="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input"
+              required
+            />
+          </div>
+
+        </div>
+       <ErrorMessage message={errorMessage} color="red" />
+        {successMessage && (
+          <div className="notification is-primary">{successMessage}</div>
+        )}
+        <br />
+        <button className="button is-primary" type="submit">
+          Send Reset Link
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
---
+++ frontend/src/bootstrap components/Login-BS.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useContext } from "react";
+import ErrorMessage from "../components/ErrorMessage";
+import { UserContext } from "../context/UserContext";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const {authToken,} = useContext(UserContext);
+  const [,setToken] = authToken;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+        const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: JSON.stringify(
+        `grant_type=&username=${email}&password=${password}&scope=&client_id=&client_secret=`
+      ),
+    };
+    const response = await fetch("/api/token", requestOptions);
+    const data = await response.json();
+
+    if (!response.ok) {
+      setErrorMessage(data.detail);
+    } else {
+      setToken(data.access_token);
+    }
+  };
+
+  return (
+    <div className="column">
+      <form className="box" onSubmit={handleSubmit}>
+        <h1 className="title has-text-centered">Login</h1>
+        <div className="field">
+          <label className="label">Email Address</label>
+          <div className="control">
+            <input
+              type="email"
+              placeholder="Enter email"
+              autoComplete="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input"
+              required
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input
+              type="password"
+              placeholder="Enter password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input"
+              required
+            />
+          </div>
+        </div>
+        <ErrorMessage message={errorMessage} />
+        <br />
+        <button className="button is-primary" type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
