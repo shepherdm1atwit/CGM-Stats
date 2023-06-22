5a40ea32c1cf3714c9e8d8549bafd6c67613d189
shepherdm <shepherdm1@wit.edu>
2023-06-21 20:59:42 -0400

    frontend can now be run in "live" mode via docker-compose-dev.yml. NOTE: currently live mode does not connect to backend, only for testing formatting/UI/UX components.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+   host: true,
+   port: 9090,
+    watch: {
+      usePolling: true
+    }
+ }
 })
