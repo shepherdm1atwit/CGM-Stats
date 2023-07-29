22248c79913fdf223992c74d54070546793db3ce
shepherdm <shepherdm1@wit.edu>
2023-07-29 15:58:12 -0400

    boxplot data should be done, needs formatting/frontend cleanup

---
+++ frontend/src/bootstrap components/BoxPlot-BS.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext";
+import Plotly from "react-plotly.js";
+
+const BoxPlot = () => {
+  const { authToken, sessionExp } = useContext(UserContext);
+  const [token, setToken] = authToken;
+  const [, setSessionExpired] = sessionExp;
+  const [plotValues, setPlotValues] = useState([]);
+  const [plotLabels, setPlotLabels] = useState([]);
+
+  useEffect(() => {
+    const getPastDayPie = async () => {
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const response = await fetch("/api/getpastweekboxplot", requestOptions);
+      const data = await response.json();
+      if (!response.ok) {
+        if (data.detail === "Your session has expired.") {
+          setSessionExpired(true);
+          setToken("null");
+        }
+      } else {
+        setPlotValues(data.values);
+        setPlotLabels(data.days);
+      }
+    };
+    getPastWeekBoxPlot();
+  }, []);
+
+
+  //TODO: unsure how to get values into boxplot, but data should be there
+  let data = [];
+  for (let i = 0; i < plotValues.length; i++) {
+    let day = {
+      y: plotValues[i],
+      type: "box",
+      name: plotLabels[i],
+    };
+    data.push(day);
+  }
+  let layout = {
+    title: "Glucose range by day in past week",
+  };
+
+  return <Plotly data={data} />;
+};
+
+export default BoxPlot;
